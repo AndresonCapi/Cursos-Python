d956f194877fa7954145f56ac82cee8e63413333
AndresonPrime <andreson.santos.t_prime@grupoboticario.com.br>
2025-01-16 21:18:49 -0300

    creat pasta do curso Python Power da Hashtag

---
+++ Curso-de-Python-3-do-basico-ao-avancado/a012_exercicios.py
@@ -0,0 +1,11 @@
+nome = 'Andreson Capi'
+altura = 1.78
+peso = 120
+imc = peso / (altura * altura)
+
+# Andreson Capi tem 1.78 de altura, 
+# pesa 120 quilos e seu IMC é
+# 40.22355
+...
+
+print(nome, 'tem', altura, 'de altura','pesa', peso, 'quilos e seu IMC é', imc)
