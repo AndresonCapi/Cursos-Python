e16e5fd782dbd6c490d611f30ce8ca1bd6f86132
AndresonPrime <andreson.santos.t_prime@grupoboticario.com.br>
2025-01-16 22:13:52 -0300

    exercicioss

---
+++ Python_Power_Hashtag/exercicio.py
@@ -0,0 +1,10 @@
+import pyautogui
+# pyautogui.click -> clicar
+# pyautogui.press -> pressionar uma tecla
+# pyautogui.write -> escrever
+
+# Passo 1: Abrir o sistema da empresa
+# Sistema: https://dlp.hashtagtreinamentos.com/python/intensivao/login
+
+
+pyautogui.press("win")
